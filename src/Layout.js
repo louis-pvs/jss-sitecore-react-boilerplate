9bfea8ddf2703dea491e132a8862589a649d9e1e
Louis Phang <design.vincent.louis@gmail.com>
2019-09-23 14:13:53 +0800

    scaffold boilerplate from jss

---
+++ src/Layout.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Placeholder,
+  VisitorIdentification
+} from "@sitecore-jss/sitecore-jss-react";
+import Helmet from "react-helmet";
+
+import "./assets/app.css";
+
+const Layout = ({ route }) => (
+  <React.Fragment>
+    <Helmet>
+      <title>
+        {(route.fields &&
+          route.fields.pageTitle &&
+          route.fields.pageTitle.value) ||
+          "Page"}
+      </title>
+    </Helmet>
+    <VisitorIdentification />
+
+    <div className="container">
+      <Placeholder name="jss-main" rendering={route} />
+    </div>
+  </React.Fragment>
+);
+
+export default Layout;
